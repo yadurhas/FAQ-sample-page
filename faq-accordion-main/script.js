af9b695451771efca514edab1b6e4308f1941a2b
Yadurhas <sahruday99@gmail.com>
2024-06-10 19:52:23 +0530

    Add needed event listeners to make the FAQs webpage navigable through keyboard.

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,6 +1,27 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const focusableElements = document.querySelectorAll('[tabindex]:not([tabindex="-1"])');
+    console.log(focusableElements);
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === "Tab") {
+            if (e.shiftKey) {
+                if (document.activeElement === focusableElements[0]){
+                    e.preventDefault();
+                    focusableElements[focusableElements.length - 1].focus();
+                }
+            }
+            else {
+                if (document.activeElement === focusableElements[focusableElements.length - 1]) {
+                    e.preventDefault();
+                    focusableElements[0].focus();
+                }
+            }
+        }
+    });
+
     const questions = Array.from(document.getElementsByClassName("question_container"));
     const parentDivs = questions.map((question) => question.parentElement);
     const answerDivs = parentDivs.map((parentDiv) => parentDiv.querySelector(".answer"));
@@ -10,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < questions.length; i++) {
         plusImage[i] = true;
         questions[i].addEventListener("click", () => {
+            //questions[i].blur();
             if(plusImage[i] === true) {
                 imageDivs[i].src = images[1];
                 answerDivs[i].style.display = "block";
@@ -19,6 +41,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 imageDivs[i].src = images[0];
                 answerDivs[i].style.display = "none";
                 plusImage[i] = true;
+            }
+        });
+        questions[i].addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                event.target.click();
             }
         });
     }
